54668a755f52c4634e684f434f7ae57cb5c5c030
Thiago Duarte <104696554+ThiagoMDuarte@users.noreply.github.com>
2024-11-11 19:19:30 -0300

    Create element Function

--- restaurant-webpack/src/index.js
+++ restaurant-webpack/src/index.js
@@ -1,1 +1,31 @@
-console.log('teste')
+/* 
+HOME TAB
+    Nome do Restaurante
+    Texto sobre o restaurante
+    Horario de funcionamento
+    Local
+
+MENU TAB
+    menu
+    categoria do prato (bebidas - entrada - principal)
+    prato
+        H- nome
+        P- descricao
+        P- valor
+
+ABOUT TAB
+    about
+    contato
+        nome
+        numero
+        email
+*/
+const contentDiv = document.querySelector('#content')
+
+function elementFactory(type,id,text) {
+    const el = document.createElement(type)
+    el.innerText = text
+    el.id = id
+    contentDiv.appendChild(el)git add .add
+    return el
+}
