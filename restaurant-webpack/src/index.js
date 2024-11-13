48ca22f14f1a8109d5d77ab437883b1d94f4cd28
Thiago Duarte <104696554+ThiagoMDuarte@users.noreply.github.com>
2024-11-13 10:42:36 -0300

    Create Menu tab func

--- restaurant-webpack/src/index.js
+++ restaurant-webpack/src/index.js
@@ -1,11 +1,5 @@
 /* 
-MENU TAB
-    menu
-    categoria do prato (bebidas - entrada - principal)
-    prato
-        H- nome
-        P- descricao
-        P- valor
+
 
 ABOUT TAB
     about
@@ -20,7 +14,6 @@ function elementFactory(type,id,text) {
     const el = document.createElement(type)
     el.innerText = text
     el.id = id
-    contentDiv.appendChild(el)
     return el
 }
 
@@ -38,5 +31,39 @@ function HomeTab () {
 
     const restaurantLocal = elementFactory('div','restaurant-local','RUA DOS RESTAURANTE 123')
 
+    contentDiv.append(
+                restaurantNameDiv,
+                restaurantText,
+                restaurantHour,
+                restaurantLocal)
 }
+
+function MenuTab () {
+    /* MENU TAB
+    menu
+    categoria do prato (bebidas - entrada - principal)
+        H- nome
+        P- descricao
+        P- valor */
+    const restaurantMenuDiv = elementFactory('div','restaurant-menu','MENU')
+
+    const restaurantDrinksDiv = elementFactory('div','food-category-bebida','BEBIDAS')
+
+    const drink1OptionTitle = elementFactory('h1','drink-title','DRINK TITLE')
+
+    const drink1OptionDescript = elementFactory('p','drink-descript','DRINK DESCRIPTION BETTER DRINK!')
+
+    const drink1OptionPrice = elementFactory('p','drink-price','$ 15.00')
+
+    restaurantDrinksDiv.append(
+            drink1OptionTitle,
+            drink1OptionDescript,
+            drink1OptionPrice)
+
+    contentDiv.append(
+            restaurantMenuDiv,
+            restaurantDrinksDiv)
+    
+}
+
 HomeTab()
